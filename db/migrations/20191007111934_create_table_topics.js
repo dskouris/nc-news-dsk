3693132395be0c06f612dea78b35a061e155e3d6
Dimitris Kouris <dskouris@gmail.com>
2019-10-07 12:07:28 +0100

    completed users table

--- db/migrations/20191007111934_create_table_topics.js
+++ db/migrations/20191007111934_create_table_topics.js
@@ -1,10 +1,12 @@
 exports.up = function(knex) {
   console.log('CREATING TOPICS TABLE');
   return knex.schema.createTable('topics', topicsTable => {
-    topicsTable.string('slug');
+    topicsTable.string('slug').primary();
+    topicsTable.string('description');
   });
 };
 
 exports.down = function(knex) {
   console.log('REMOVING TOPICS TABLE');
+  return knex.schema.dropTable('topics');
 };
---
+++ db/migrations/20191007115410_create_table_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  console.log('creating users table');
+  return knex.schema.createTable('users', usersTable => {
+    usersTable
+      .string('username')
+      .primary()
+      .unique();
+    usersTable.string('avatar_url');
+    usersTable.string('name');
+  });
+};
+
+exports.down = function(knex) {
+  console.log('removing users table');
+  return knex.schema.dropTable('users');
+};
