ae51d71094350a09d0099ad671cca394f9d747ca
Dimitris Kouris <dskouris@gmail.com>
2019-10-11 11:37:53 +0100

    removed console log

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const apiRouter = require('./routers/apiRouter');
 const { handleCustomErrors, handlePSQLErrors } = require('./errors/index');
 
 app.use(express.json());
-app.use('/api', (req, res, next) => {
-  console.log('requested');
-  next;
-});
+
 app.use('/api', apiRouter);
 app.use(handleCustomErrors);
 app.use(handlePSQLErrors);
